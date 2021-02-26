77f4ffb29404cda81148d32cf71afbd5b71e315a
ayyle <nborchers1@gmail.com>
2021-02-26 02:40:52 -0500

    added statistics and avg calculator

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,9 +6,12 @@ const Display = ({counter, text}) =>{
    return( <div> {text} {counter} </div>)
 } 
 
-const Button = ({handleClick, text}) =>(
-  <button onClick={handleClick}> {text} </button>
+const Button = ({onClick, text}) =>{
+  
+  return(
+  <button onClick={onClick}> {text} </button>
 )
+  }
 
 
 const App = () => {
@@ -16,20 +19,33 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleGood = () =>{ setGood(good + 1); setAll(allClicks.concat(1))}
+  const handleNeutral = () =>{setNeutral(neutral + 1); setAll(allClicks.concat(0))}
+  const handleBad = () =>{setBad(bad + 1); setAll(allClicks.concat(-1))}
+  const getAvg = (allClicks) => {
+    const total =allClicks.reduce((acc, c) => acc+ c, 0);
+    return total / allClicks.length;
+  }
+
+
 
   return (
     <div>
       <div>
         <h1>give feedback</h1>
-        <Button handleClick={() => setGood(good + 1)} text="good"/>
-        <Button handleClick={() => setNeutral(neutral + 1)}text="neutral"/>
-        <Button handleClick={() => setBad(bad + 1)} text="bad"/>
+        <Button onClick={handleGood} text="good"/>
+        <Button onClick={handleNeutral}text="neutral"/>
+        <Button onClick={handleBad} text="bad"/>
       </div>
       <div>
         <h1>statistics</h1>
         <Display counter={good} text="good"/>
         <Display counter={neutral} text="neutral"/>
         <Display counter={bad} text="bad"/>
+        <Display counter={allClicks.length} text="all"/>
+        <Display counter={getAvg(allClicks)} text="average" />
       </div>
     </div>
   )
